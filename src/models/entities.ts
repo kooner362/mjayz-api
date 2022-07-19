5da7e8a7179e40a2a79920dba0b70200d59c7c72
Gurpreet Kooner <kooner@gmail.com>
2022-07-18 21:02:47 -0700

    added remaining models

--- src/models/entities.ts
+++ src/models/entities.ts
@@ -3,7 +3,7 @@ import { sequelize } from "../utils/dbUtil"
 
 export const Entities = sequelize.define('Entities', {
   id: { type: DataTypes.UUIDV4, allowNull: false, primaryKey: true },
-  parentEntityId: { type: DataTypes.STRING, allowNull: true },
+  parentEntityId: { type: DataTypes.STRING, allowNull: true, defaultValue: null },
   entityType: { type: DataTypes.STRING, allowNull: false }
 })
 
--- src/models/entityUsers.ts
+++ src/models/entityUsers.ts
@@ -3,8 +3,8 @@ import { sequelize } from "../utils/dbUtil"
 import { User } from "./user"
 
 export const EntityUsers = sequelize.define('EntityUsers', {
-  entityId: { type: DataTypes.UUIDV4, allowNull: false, primaryKey: true, references: { model: User, key: 'id' } },
-  userId: { type: DataTypes.STRING, allowNull: true },
+  entityId: { type: DataTypes.UUIDV4, allowNull: false, primaryKey: true },
+  userId: { type: DataTypes.STRING, allowNull: false, references: { model: User, key: 'id' }  },
 })
 
 export interface IEntityContract {
